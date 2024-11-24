e1f37ffba0e1d20dcd5e947665560b77298651a5
Corium4 <kowalski.wiktor256@gmail.com>
2024-11-24 12:48:45 +0100

    main

--- app.py
+++ app.py
@@ -5,21 +5,14 @@ from defined_arrays import defined_rgb_values_NDVI as NDVI, defined_rgb_values_B
 from number_to_image import process_number_to_image as nb_to_img
 from image_processing import image_processing as img_p
 
-#Img_dir = "Visual_data/2017-07-17-00_00_2017-07-17-23_59_Sentinel-2_L2A_Normalized_Difference_Vegetation_Index_(NDVI).png"
-#Img_dir = "Visual_data/2017-07-17-00_00_2017-07-17-23_59_Sentinel-2_L2A_False_Color.png"
-#Img_dir = "Visual_data/2017-07-17-00_00_2017-07-17-23_59_Sentinel-2_L2A_Burned_Area_Detection.png"
+
 
 Img_dir = "Visual_data/NFVI_test.png"
-image = Image.open(Img_dir)
-#image.show()
-image = image.convert("RGB")
-image_array = np.array(image)
-
 
 #img_p(image_array,NDVI)
-#img_to_nb(image_array,NDVI)
+img_to_nb(Img_dir,NDVI)
 
 
-txt_filename = 'output.txt'  # The text file containing the array of numbers
-nb_to_img(txt_filename, NDVI)
+#txt_filename = 'output.txt'  # The text file containing the array of numbers
+#nb_to_img(txt_filename, NDVI)
 
